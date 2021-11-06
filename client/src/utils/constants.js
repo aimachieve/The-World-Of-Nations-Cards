0ba8245b7219b93525dcf121df26f919b90755e6
unknown <honguyen.official@gmail.com>
2021-11-06 12:52:51 -0700

    updated contact us email notifier-2

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,4 +1,4 @@
-// export const SERVER_UPLOAD_URL = `http://${window.location.hostname}:5000/uploads/`  //  For development
-export const SERVER_UPLOAD_URL = `http://${window.location.hostname}/uploads/` //  For host
+export const SERVER_UPLOAD_URL = `http://${window.location.hostname}:5000/uploads/`  //  For development
+// export const SERVER_UPLOAD_URL = `http://${window.location.hostname}/uploads/` //  For host
 export const SAMPLE_USER_EMAIL = 'maverick.sin.0509@gmail.com'
 export const SAMPLE_USER_EMAIL_PASSWORD = 'dkaneh ahffk.'
